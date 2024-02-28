23a813ab36fa5c3f3b08bb32f30e42fb353de8f6
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-02-28 10:21:32 +0100

    feat: etape 1 et 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,45 @@
+function selectColor(event) {
+  let colorBox = event.target
+  let color = colorBox.style.backgroundColor
 
-function selectColor(event)
-{
-    let colorBox = event.target;
-    let color = colorBox.style.backgroundColor;
-
-    sessionStorage.setItem("selectedColor", color);
+  sessionStorage.setItem('selectedColor', color)
 }
 
-function getSelectedColor()
-{
-    if(sessionStorage.getItem("selectedColor"))
-    {
-        return sessionStorage.getItem("selectedColor");
-    }
+function getSelectedColor() {
+  if (sessionStorage.getItem('selectedColor')) {
+    return sessionStorage.getItem('selectedColor')
+  }
 
-    return null;
+  return null
 }
 
-function loadPalette(palette)
-{
-    let colors = palette;
-
-    // le code de l'étape 1 se passe ici
+function loadPalette(palette, headerDivs) {
+  let colors = palette
+  for (i = 0; i < colors.length; i++) {
+    headerDivs[i].style.backgroundColor = colors[i]
+  }
 }
 
+window.addEventListener('DOMContentLoaded', function () {
+  let headerDivs = document.querySelectorAll('header div')
+  loadPalette(
+    [
+      '#22f6f3',
+      '#3daf7e',
+      '#ffffff',
+      '#ec8236',
+      '#a9a7ee',
+      '#ecc606',
+      '#f783f2',
+      '#e89e80',
+    ],
+    headerDivs
+  )
 
-window.addEventListener("DOMContentLoaded", function(){
-    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
+  // le code de l'étape 2 se passe ici
+  headerDivs.forEach((div) => {
+    div.addEventListener('click', selectColor)
+  })
 
-    // le code de l'étape 2 se passe ici
-
-
-    // le code de l'étape 3 se passe ici
-
-});
+  // le code de l'étape 3 se passe ici
+})
